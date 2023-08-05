76d789f245051db4fd737b3fdace9df894332335
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2023-08-05 23:15:36 +0300

    Created Insert method

--- apicustom/public/index.php
+++ apicustom/public/index.php
@@ -1,37 +1,49 @@
 <?php
 global $CoreParams;
-require_once ("../config/config.php");
-spl_autoload_register(function ($className){
+require_once("../config/config.php");
+spl_autoload_register(function ($className) {
     $path = "../src/{$className}.php";
-    if(file_exists($path)){
-        require_once ($path);
+    if (file_exists($path)) {
+        require_once($path);
     }
 });
 
 
-$database = new Database($CoreParams['Database']['Host'], $CoreParams['Database']['Username'],$CoreParams['Database']['Password'], $CoreParams['Database']['Database']);
+$database = new Database($CoreParams['Database']['Host'], $CoreParams['Database']['Username'], $CoreParams['Database']['Password'], $CoreParams['Database']['Database']);
 $database->connect();
 $query = new QueryBuilder();
 
 
-$query->select(["title", "text"])
-    ->from('news')
-->where(["id"=>5]);
-$rows = $database->execute($query);
+//$query->select(["title", "text"])
+//    ->from('news')
+//    ->where(["id" => 5]);
+//$rows = $database->execute($query);
 
-//$pdo = new PDO("mysql:host=172.22.75.8;dbname=cms","student", "root");
-//$sth=$pdo->prepare("SELECT * FROM news WHERE id= :id");
-//$sth->execute([4]);
-//$rows = $sth->fetchAll();
-//var_dump($rows);
-//$front_controller = new FrontController();
-//$front_controller->run();
+//$query->insert(
+//    [
+//        [
+//            "title" => 'title',
+//            "text" => "check1",
+//            "date" => "2023-01-12"
+//        ],
+//        [
+//            "title" => 'title',
+//            "text" => "check2",
+//            "date" => "2023-01-12"
+//        ]
+//    ]
+//)->into("news");
+//$database->execute($query);
+
+
+
+//echo $query->getSql();
 
 $CoreParams['Database'] = [
-    "Host"=>"172.22.75.8",
-    "Username"=>"student",
-    "Password"=>"root",
-    "Database"=>"cms"
+    "Host" => "172.22.75.8",
+    "Username" => "student",
+    "Password" => "root",
+    "Database" => "cms"
 ];
 
 
--- apicustom/src/QueryBuilder.php
+++ apicustom/src/QueryBuilder.php
@@ -1,5 +1,7 @@
 <?php
-class QueryBuilder{
+
+class QueryBuilder
+{
 
     protected $fields;
     protected $table;
@@ -7,51 +9,119 @@ class QueryBuilder{
     protected $params;
     protected $where;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->params = [];
     }
 
-    public function select ($fields="*"){
+    public function select($fields = "*")
+    {
         $this->type = "select";
         $fields_string = $fields;
-        if(is_array($fields)){
+        if (is_array($fields)) {
             $fields_string = implode(", ", $fields);
         }
         $this->fields = $fields_string;
         return $this;
     }
 
-
-    public function from($table){
+    public function from($table)
+    {
         $this->table = $table;
         return $this;
     }
 
-    public function getSql(){
-        switch ($this->type){
+    public function getSql()
+    {
+        switch ($this->type) {
             case 'select':
                 $sql = "SELECT {$this->fields} FROM {$this->table}";
-                if(!empty($this->where)){
+                if (!empty($this->where)) {
                     $sql .= " WHERE {$this->where}";
                 }
+                return $sql;
+            case 'insert':
+                $sql = "INSERT INTO  {$this->table} ({$this->fields['keys']}) VALUES {$this->fields['keys_prepared_query']}";
                 return $sql;
         }
     }
 
-    public function where($where) {
-    /*    if(is_a($where)){
-
-        }*/
+    public function where($where)
+    {
         $where_parts = [];
-        foreach ($where as $key=>$value){
+        foreach ($where as $key => $value) {
             $where_parts[] = "{$key} = :{$key}";
-            $this->params[$key]=$value;
+            $this->params[$key] = $value;
         }
         $this->where = implode(" AND ", $where_parts);
         return $this;
     }
 
-    public function getParams(){
+    public function getParams()
+    {
         return $this->params;
     }
+
+
+    public function into($table)
+    {
+        $this->table = $table;
+        return $this;
+    }
+
+
+    public function insert($fields)
+    {
+        $this->type = "insert";
+        $counter = 0;
+        $insert_keys = [];
+        $keys_prepared_query = [];
+
+        function is_two_dimensional_array($array) {
+            foreach ($array as $element) {
+                if (is_array($element)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        if (is_two_dimensional_array($fields)) {
+            foreach ($fields as $key => $value) {
+                foreach ($value as $sub_key => $sub_value) {
+                    if (!in_array($sub_key, $insert_keys)) {
+                        $insert_keys[] = $sub_key;
+                    }
+                    if ($sub_key == array_keys($value)[0]) {
+                        $keys_prepared_query[] .= "( :{$sub_key}" . $counter;
+                    } elseif ($sub_key == array_keys($value)[count($value)-1]) {
+                        $keys_prepared_query[] .= ":{$sub_key}" . $counter . " )";
+                    } else {
+                        $keys_prepared_query[] .= ":{$sub_key}" . $counter;
+                    }
+                    $this->params[$sub_key.$counter] = $sub_value;
+                }
+                $counter++;
+            }
+        } else {
+            foreach ($fields as $key => $value) {
+                $insert_keys[] = $key;
+
+                if ($key == array_keys($fields)[0]) {
+                    $keys_prepared_query[] .= "( :{$key}";
+                } elseif ($key == array_keys($fields)[count($fields)-1]) {
+                    $keys_prepared_query[] .= ":{$key} )";
+                } else {
+                    $keys_prepared_query[] .= ":{$key}";
+                }
+                $this->params[$key] = $value;
+            }
+        }
+
+        $this->fields['keys'] = implode(", ", $insert_keys);
+        $this->fields['keys_prepared_query'] = implode(", ", $keys_prepared_query);
+        return $this;
+    }
+
+
 }
