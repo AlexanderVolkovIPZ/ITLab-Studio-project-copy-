921bd06d1ae67253530f07761bc0b3c245f703d9
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2023-09-04 01:16:44 +0300

    Update project

--- frontend/src/components/Counter/index.js
+++ frontend/src/components/Counter/index.js
@@ -1,17 +1,75 @@
 import {useState} from "react"
+import styled, {css} from "styled-components";
 import PropTypes from "prop-types";
-function Counter({value = 20, padding = 20, color = "red"}){
-    const [currentValue,setCurrentValue]= useState(value||0)
-    return <div style={{padding, color:color}}>
-        <div>Value {currentValue}</div>
-        <div>
-            <button onClick={()=>setCurrentValue(currentValue+1)}>+</button>
-            <button onClick={()=>setCurrentValue(currentValue-1)}>-</button>
-        </div>
-    </div>
+
+let ValueStyled = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  ${({min, max, value}) => {
+    if (min > value || max < value) {
+      return `color:red;`;
+    } else {
+      return `color:green;`;
+    }
+  }}
+`
+
+let ButtonsWrapper = styled.div`
+  display: inline-block;
+    & input{
+      font-size: 30px;
+      display: inline-block;
+      width: 50px;
+      height: 50px;
+    }
+  & input:nth-child(1){
+    background-color: yellow;
+  }
+  & input:last-child{
+    background-color: deepskyblue;
+  }
+`
+
+let ContentWrapper = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+function Counter({value = 20, padding = 20, color = "green", min = -100, max = 100}) {
+    const [currentValue, setCurrentValue] = useState(value || 0)
+
+    const updateStyle = () => {
+        const style = window.getComputedStyle(document.getElementById("value"));
+        if (style.color === "red") {
+            return "green";
+        } else {
+            return "red";
+        }
+    };
+
+    const handleIncrement = () => {
+        let valueStyled = document.getElementById('value');
+        let valueIncrement = currentValue + 1
+        setCurrentValue(valueIncrement)
+        valueStyled.style.color = valueIncrement < min || valueIncrement > max ? "red" : "green";
+    }
+    const decrementIncrement = () => {
+        let valueStyled = document.getElementById('value');
+        let valueDecrement = currentValue - 1
+        setCurrentValue(valueDecrement)
+        valueStyled.style.color = valueDecrement < min || valueDecrement > max ? "red" : "green";
+    }
+
+    return <ContentWrapper style={{padding, color: color}}>
+        <ValueStyled min={min} max={max} value={value} id="value">Value {currentValue}</ValueStyled>
+        <ButtonsWrapper>
+            <input type="button" value="+" onClick={handleIncrement}/>
+            <input type="button" value="-" onClick={decrementIncrement}/>
+        </ButtonsWrapper>
+    </ContentWrapper>
 }
 
-/*Counter.defaultProps = {
-    value:100
-}*/
 export default Counter
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,25 +12,23 @@ let headerContent = {
     "price": "Ціни",
     "another": "Інше",
     "anotherContent": {
-        "aboutUs":"Про нас",
-        "ourHistory":"Наша історія",
-        "charity":"Благодійність",
+        "aboutUs": "Про нас",
+        "ourHistory": "Наша історія",
+        "charity": "Благодійність",
     }
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const arr = [3,12,45,656,87,9, 50]
+const values = {
+    "min": 0,
+    "max": 20,
+    "currentValue":10
+}
 root.render(
     <React.StrictMode>
         {
-            arr.map((value, index)=>
-                <Counter key = {index} value = {value} color="brown"/>
-            )
+            <Counter min = {values.min} max = {values.max} value={values.currentValue} color="green"/>
         }
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
